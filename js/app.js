48b4d808f5284d7a6ae117481f747cb86a805867
MDTaiyaburRahmanTuhin <mdtaiyaburrahmantuhin99>
2021-09-13 15:57:31 +0600

    rating count update

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ const showProducts = (products) => {
       <i class="fas fa-star filled"></i>
       <i class="fas fa-star filled"></i>
       <i class="fas fa-star empty"></i></p>
+      <p>rating count: ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
